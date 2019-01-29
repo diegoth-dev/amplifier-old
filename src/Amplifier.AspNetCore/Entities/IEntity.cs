37d9d75b3b945d0aee69404bcd75f39bd2d1b071
FabriDamazio <fabridamazio@gmail.com>
2019-01-29 17:43:17 -0200

    Add Entity base class
    Entity class override equals, ==, != and GetHashCode to implement identifier equality.

--- src/Amplifier.AspNetCore/Entities/IEntity.cs
+++ src/Amplifier.AspNetCore/Entities/IEntity.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Implement this interface to define an entity for the application.
     /// </summary>
-    /// <typeparam name="TPrimaryKey"></typeparam>
+    /// <typeparam name="TPrimaryKey">Entity primary key type.</typeparam>
     public interface IEntity<TPrimaryKey>
     {
         /// <summary>
