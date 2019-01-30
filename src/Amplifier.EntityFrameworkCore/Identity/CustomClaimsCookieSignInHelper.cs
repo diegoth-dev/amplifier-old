5bb03701cb62434a5826096a19b5659eebf915ae
FabriDamazio <fabridamazio@gmail.com>
2019-01-30 15:51:31 -0200

    Add CustomClaimsCookieSignInHelper

---
+++ src/Amplifier.EntityFrameworkCore/Identity/CustomClaimsCookieSignInHelper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Amplifier.EntityFrameworkCore.Identity
+{
+    /// <summary>
+    /// Custom SignIn handler. Let add custom claims. 
+    /// </summary>
+    /// <typeparam name="TIdentityUser">Application user class.</typeparam>
+    /// <typeparam name="TKey">User class primary key type</typeparam>
+    public class CustomClaimsCookieSignInHelper<TIdentityUser, TKey> : ICustomClaimsCookieSignInHelper<TIdentityUser, TKey> 
+        where TIdentityUser : IdentityUser<TKey> 
+        where TKey : IEquatable<TKey>
+    {
+        private readonly SignInManager<TIdentityUser> _signInManager;
+
+        /// <summary>
+        /// CustomClaimsCookieSignInHelper constructor.
+        /// </summary>
+        /// <param name="signInManager"></param>
+        public CustomClaimsCookieSignInHelper(SignInManager<TIdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        /// <summary>
+        /// Method for SignIn.
+        /// </summary>
+        /// <param name="user">The user.</param>
+        /// <param name="isPersistent">Set whether the authentication session is persisted across multiple requests.</param>
+        /// <param name="customClaims">Custom claims.</param>
+        /// <returns></returns>
+        public async Task SignInUserAsync(TIdentityUser user, bool isPersistent, IEnumerable<Claim> customClaims)
+        {
+            var claimsPrincipal = await _signInManager.CreateUserPrincipalAsync(user);
+            if (customClaims != null && claimsPrincipal?.Identity is ClaimsIdentity claimsIdentity)
+            {
+                claimsIdentity.AddClaims(customClaims);
+            }
+            await _signInManager.Context.SignInAsync(IdentityConstants.ApplicationScheme,
+                claimsPrincipal,
+                new AuthenticationProperties { IsPersistent = isPersistent });
+        }
+    }
+}
---
+++ src/Amplifier.EntityFrameworkCore/Identity/ICustomClaimsCookieSignInHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Amplifier.EntityFrameworkCore.Identity
+{
+    /// <summary>
+    /// Interface for CustomClaimsCookieSignInHelper class
+    /// </summary>
+    /// <typeparam name="TIdentityUser">Application user class.</typeparam>
+    /// <typeparam name="TKey">User primary key type.</typeparam>
+    public interface ICustomClaimsCookieSignInHelper<TIdentityUser, TKey> 
+        where TIdentityUser : IdentityUser<TKey> 
+        where TKey : IEquatable<TKey>
+    {
+    }
+}
