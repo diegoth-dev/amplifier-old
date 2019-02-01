4f8b4cba0c2a882de9cf7f183d921c4b78bf0766
FabriDamazio <fabridamazio@gmail.com>
2019-02-01 14:52:34 -0200

    Add AmplifierClaimManager

---
+++ src/Amplifier.AspNetCore/Authentication/AmplifierClaimManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Amplifier.AspNetCore.Authentication
+{
+    /// <summary>
+    /// Provides the APIs to generate Amplifier default claims.
+    /// </summary>
+    public class AmplifierClaimManager
+    {
+        /// <summary>
+        /// Generate default Amplifier claims.
+        /// </summary>
+        /// <param name="userId">User unique identifier.</param>
+        /// <param name="userName">User name.</param>
+        /// <param name="tenantId">Tenant unique identifier.</param>
+        /// <param name="userRoles">User roles list.</param>
+        /// <returns></returns>
+        public Claim[] GenerateDefaultClaims(string userId, string userName, string tenantId, IList<string> userRoles)
+        {
+            if(string.IsNullOrEmpty(userId) && string.IsNullOrEmpty(userName) && string.IsNullOrEmpty(tenantId))
+            {
+                throw new ArgumentNullException("Claim cannot be null");
+            }
+            var amplifierClaims = new[]
+            {
+                new Claim("userid", userId, ClaimValueTypes.Integer),
+                new Claim("username", userName, ClaimValueTypes.String),
+                new Claim("tenantid", tenantId, ClaimValueTypes.Integer),
+            };
+
+            IEnumerable<Claim> rolesClaim = userRoles.Select(rn => new Claim("roles", rn, ClaimValueTypes.String));
+            amplifierClaims.Append(new Claim("roles", rolesClaim.ToString(), ClaimValueTypes.String));
+
+            return amplifierClaims;
+        }        
+    }
+}
